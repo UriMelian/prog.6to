cfa2a7a86942cd3c8f3d5b5942d6901e6407cb25
UriMelian <Urimelian28@gmail.com>
2026-03-04 15:48:40 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,10 @@ DB_HOST = (
 "aws-0-us-west-2.pooler.supabase.com"  #  Aca va el host de tu base de datos
 )
 DB_PORT = "6543"  #  Este es siempre el mismo
-DB_NAME = "APIgd"  #  Aca va el nombre de tu base de datos
-DB_USER = "UriMeliaen's Org"  #  Aca va el usuario de tu base de datos
+DB_NAME = "postgres"  #  Aca va el nombre de tu base de datos
+DB_USER = "postgres.sueorzurbufficudkoji"  #  Aca va el usuario de tu base de datos
 DB_PASSWORD = "o67FFy0X6rlVPVbY"  #  Aca va la contraseña de tu base de datos
 DB_SSLMODE = "require"  #  Este siempre es lo mismo
+
 
 
